5530ae0ac79325ed91e4bd6aeaac5c43b81699ba
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-04-28 00:52:03 +0700

    Add messages array in indexRouter

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,19 @@ const { Router } = require("express");
 const indexRouter = Router();
 const indexController = require("../controllers/indexController");
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 indexRouter.get("/", indexController.get);
 
 module.exports = indexRouter;
