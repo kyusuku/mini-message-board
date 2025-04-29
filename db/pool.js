abab5dec6fc0d5b7d396d559a21947666aa66c84
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-04-30 01:57:41 +0700

    Reconnect with database

--- db/pool.js
+++ db/pool.js
@@ -3,4 +3,4 @@ const { Pool } = require("pg");
 
 const connectionString = process.env.DATABASE_URL;
 
-postgresql: module.exports = new Pool({ connectionString });
+module.exports = new Pool({ connectionString });
