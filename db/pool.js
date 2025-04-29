b1c8a8d23a4f060ceb20e58d6d4b51044548dc8d
kyusuku <147250847+kyusuku@users.noreply.github.com>
2025-04-30 01:45:09 +0700

    Fix some bugs

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,6 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const connectionString =
-  "postgresql://postgres:zedQYEJaxUjiLkciKvIiEmGfZhOoUYTv@tramway.proxy.rlwy.net:28667/railway";
-
-console.log("Database URL: ", connectionString);
-//postgres:zedQYEJaxUjiLkciKvIiEmGfZhOoUYTv@tramway.proxy.rlwy.net:28667/railway
-// ||
-// `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE}`;
+const connectionString = process.env.DATABASE_URL;
 
 postgresql: module.exports = new Pool({ connectionString });
--- db/populatedb.js
+++ db/populatedb.js
@@ -14,10 +14,7 @@ async function main() {
   console.log("seeding...");
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
-    // ||
-    // `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE}`,
   });
-  console.log("Database URL (populatedb): ", process.env.DATABASE_URL);
   await client.connect();
   await client.query(SQL);
   await client.end();
